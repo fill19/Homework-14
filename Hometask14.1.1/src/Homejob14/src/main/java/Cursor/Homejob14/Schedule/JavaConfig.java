ac88f6db4c74ece28a5e4952a85d2d8695c8e571
Dmytro <65898747+fill19@users.noreply.github.com>
2021-02-10 20:09:53 +0200

    Update JavaConfig.java

--- Hometask14.1.1/src/Homejob14/src/main/java/Cursor/Homejob14/Schedule/JavaConfig.java
+++ Hometask14.1.1/src/Homejob14/src/main/java/Cursor/Homejob14/Schedule/JavaConfig.java
@@ -1,11 +1,11 @@
 package Cursor.Homejob14.Schedule;
 
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.ComponentScan;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.scheduling.TaskScheduler;
-        import org.springframework.scheduling.annotation.EnableScheduling;
-        import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 
 @EnableScheduling
 @Configuration
@@ -14,3 +14,5 @@ public class JavaConfig {
     @Bean
     TaskScheduler scheduler() {
         return new ConcurrentTaskScheduler();
+    }
+}
